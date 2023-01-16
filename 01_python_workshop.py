54934cff06ad9afd9e5671ee5dbc94e72a61d476
Gobro-s <gobro6366@gmail.com>
2023-01-16 22:09:14 +0900

    01_python_workshop

---
+++ 01_python_workshop.py
@@ -0,0 +1,18 @@
+#1. 문자 print
+a = "It's SSAFY8"
+print(a)
+
+#2. 숫자 print
+a = 458345
+b = 623576
+print(a + b)
+
+#3. 변수를 사용해서 데이터 출력하기
+greeting = 'Hello'
+month = 'July'
+print(greeting + month)
+
+#4. 문자형의 입력과 출력
+>>>hello = input()
+Hello! SSAFY8
+>>>hello
