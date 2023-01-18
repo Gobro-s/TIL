87883327198ba8955da97ef3788f9d3ad8f2338a
Gobro-s <gobro6366@gmail.com>
2023-01-18 22:38:21 +0900

    백준 10430번 문제 풀기

---
+++ Baekjoon10430.py
@@ -0,0 +1,12 @@
+#(A+B)%C == ((A%C) + (B%C))%C  ?
+#(A*B)%C == ((A%C)*(B%C))%C  ?
+
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+print((a+b)%c)
+print(((a%c) + (b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
