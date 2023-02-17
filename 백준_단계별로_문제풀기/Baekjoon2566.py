9678d99ccca23a69d81bad1c40b0f8f8e68ac33c
Gobro-S <gobro6366@gmail.com>
2023-02-17 16:18:40 +0900

    백준2566,2차원배열

---
+++ 백준_단계별로_문제풀기/Baekjoon2566.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/2566
+
+arr = [[0] for _ in range(9)]
+
+for i in range(9):
+    arr[i] = list(map(int, input().split()))
+
+# print(arr) # [[],[],[]...] 형태로 input()
+
+# 0 혹은 양수만 입력되므로 최댓값을 -1로 초기 설정
+mx = -1
+# 행, 열 == 0부터 시작
+X = 0
+Y = 0
+
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] > mx:  # 만약 arr의 [i]행 [j]열이 최댓값보다 크다면
+            mx = arr[i][j]  # mx에 새로운 최댓값을 할당
+            X = i+1         # 그때의 index(i) + 1
+            Y = j+1         # 그때의 index(j) + 1
+
+print(mx)
+print(X, Y, end = '')
