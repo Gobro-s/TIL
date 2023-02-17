98c8f59b94002b7ae5d146415b964da46e78075b
Gobro-S <gobro6366@gmail.com>
2023-02-17 16:53:16 +0900

    0217백준2738

--- 백준_단계별로_문제풀기/Baekjoon2738.py
+++ 백준_단계별로_문제풀기/Baekjoon2738.py
@@ -2,19 +2,17 @@
 
 N, M = map(int, input().split())
 
-A = [0 for _ in range(N)]
-B = [0 for _ in range(M)]
+A = [[0] for _ in range(N)]
+B = [[0] for _ in range(N)]
 for i in range(N):
     A[i] = list(map(int, input().split()))
 
-for j in range(M):
+for j in range(N):
     B[j] = list(map(int, input().split()))
 
-print(A)
-print(B)
-# for i in range(N):
-#     for j in range(M):
-#         print(A[i][j] + B[i][j], end = ' ')
+for r in range(N):
+    for c in range(M):
+        print(A[r][c] + B[r][c], end = ' ')
 
 
-#     print()
+    print()
