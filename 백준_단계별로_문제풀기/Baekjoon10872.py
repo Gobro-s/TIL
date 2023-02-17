91874992df1c1c3b5dde41c42749980e36e47bda
Gobro-S <gobro6366@gmail.com>
2023-02-17 17:57:06 +0900

    0217백준10872풀다만거

---
+++ 백준_단계별로_문제풀기/Baekjoon10872.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/10872
+
+# def fact(n):
+#     nfact = 0
+#     for i in range(n):
+#         if index(3) > 1:
+#             nfact += i*(i-1)
+#         elif n[i] == 1:
+
+
+n = [1, 2, 3, 4, 5, 6]
+
+n3 = n.index(3)
+
+print(n3)
