ee517f340a5131ba25c9da2691fc38c97665d04d
Gobro-S <gobro6366@gmail.com>
2023-02-27 15:45:50 +0900

    0227백준5086

---
+++ 백준_단계별로_문제풀기/Baekjoon5086.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/5086
+
+
+while True:
+    num1, num2 = map(int, input().split())
+    
+    if num1 == num2 == 0:
+        break
+    
+    elif num2 % num1 == 0:
+        print('factor')
+    
+    elif num1 % num2 == 0:
+        print('multiple')
+
+    else:
+        print('neither')
