546dca78f83ef6ecd2a1b5b611b6aafbe462d314
Gobro-S <gobro6366@gmail.com>
2023-03-02 17:56:57 +0900

    0302

--- 백준_단계별로_문제풀기/Baekjoon1978.py
+++ 백준_단계별로_문제풀기/Baekjoon1978.py
@@ -1,15 +1,30 @@
 # https://www.acmicpc.net/problem/1978
 
-N = int(input())
-for i in range(N):
+# N = int(input())
+# for i in range(N):
 
+#     lst = []
+#     cnt = 0
+#     num = map(int, input().split())
+#     for j in range(num):
+#         if num % j == 0:
+#             lst.append(j)
+#             if len(lst) == 2:
+#                 cnt += 1
+
+#     print(cnt)
+
+
+N= int(input())
+num = list(map(int, input().split()))
+for i in range(N):
     lst = []
     cnt = 0
-    num = map(int, input().split())
     for j in range(N):
         if num % j == 0:
             lst.append(j)
+        
             if len(lst) == 2:
                 cnt += 1
 
-    print(cnt)
+    print(cnt) 
