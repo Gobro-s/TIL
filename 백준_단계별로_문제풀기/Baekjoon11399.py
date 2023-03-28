1bf3386c57da15f7c6b78fa85cb350f4b0b1486e
Gobro <gobro6366@gmail.com>
2023-03-28 13:52:30 +0900

    0328greedy

--- 백준_단계별로_문제풀기/Baekjoon11399.py
+++ 백준_단계별로_문제풀기/Baekjoon11399.py
@@ -6,10 +6,10 @@ n = int(input())
 time = list(map(int, input().split()))
 
 
-time.sort()
+time.sort()  # 대기 시간이 짧을 수록 빨리 뽑을 수 있음
 
 total = 0
 for i in range(1, n+1):
-    total += sum(time[0:i])
+    total += sum(time[0:i])  # 걸린 시간을 총 더해준다
 
 print(total)
