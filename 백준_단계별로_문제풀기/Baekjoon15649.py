0df1bc76a4d3ebe3624f39170dacac0d8da4d42d
Gobro <gobro6366@gmail.com>
2023-03-31 12:18:45 +0900

    0331백준 백트래킹

---
+++ 백준_단계별로_문제풀기/Baekjoon15649.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/15649
+
+# 자연수 N과 M이 주어짐
+# 1부터 N까지 자연수 중 중복 없이 M개 고른 수열
+nums = []
+def f():
+    if len(nums) == M:     # 주어진 M과 수열의 길이가 같다면
+        print(*nums)       # print 하쇼
+        return
+    for i in range(1, N+1):   # 1부터 N까지 도는동안
+        if i not in nums:     # i가 nums에 없다면
+            nums.append(i)    # 추가 해보고
+            f()               # 함수 재 호출 하고
+            nums.pop()        # 계속 쌓이지 않게 빼줘
+
+
+
+N, M = map(int, input().split())
+
+f()
