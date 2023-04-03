6e335e4389181cd0f4767a153f396a7dee5c6bc8
Gobro <gobro6366@gmail.com>
2023-04-03 16:15:52 +0900

    0403

--- 백준_단계별로_문제풀기/Baekjoon1018.py
+++ 백준_단계별로_문제풀기/Baekjoon1018.py
@@ -2,9 +2,29 @@
 
 N, M = map(int, input().split())
 
-arr = [list(map(str,input())) for _ in range(N)]
+arr = [input() for _ in range(N)]
 
-cnt = 0
-for i in range(M):
-    for j in range(N):
-        if arr[0][0] == 'W':
+
+res = []
+
+for i in range(N-7):         # 8X8로 잘라낸다고 했으니까
+    for j in range(M-7):
+        draw_W = 0   # 하얀색으로 시작하는 판
+        draw_B = 0   # 깜장색으로 시작하는 판
+        for x in range(i, i+8):
+            for y in range(j, j+8):
+                if (x+y)%2:
+                    if arr[x][y] != 'W':
+                        draw_W += 1
+                    else:
+                        draw_B += 1
+                else:
+                    if arr[x][y] != 'B':
+                        draw_W += 1
+                    else:
+                        draw_B += 1
+
+        res.append(draw_B)
+        res.append(draw_W)
+
+print(min(res))
