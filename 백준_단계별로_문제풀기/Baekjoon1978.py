139a2ed198946b84921179826c73aa3a5c2bfa19
Gobro-s <gobro6366@gmail.com>
2023-04-06 22:24:36 +0900

    0406

--- 백준_단계별로_문제풀기/Baekjoon1978.py
+++ 백준_단계별로_문제풀기/Baekjoon1978.py
@@ -17,14 +17,13 @@
 
 N= int(input())
 num = list(map(int, input().split()))
-for i in range(N):
-    lst = []
-    cnt = 0
-    for j in range(N):
-        if num % j == 0:
-            lst.append(j)
-        
-            if len(lst) == 2:
-                cnt += 1
 
-    print(cnt) 
+ans=0
+for i in num:
+    cnt=0
+    for j in range(2, i+1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 1:
+        ans += 1
+print(ans)
