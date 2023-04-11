4074614f19b77e8d2ed8fe2fe60e9045c634f1ae
친절한 고형 <122508622+Gobro-s@users.noreply.github.com>
2023-04-11 17:42:41 +0900

    Delete learning-algo directory

--- learning-algo/0201/view.py
+++
@@ -1,1 +0,0 @@
-_map = [[0 for _ in range(1000)] for _ in range(255)]
--- learning-algo/0328_완전검색&그리디/binary_subset.py
+++
@@ -1,9 +0,0 @@
-arr = [3, 6, 7, 1, 5, 4]
-n = len(arr)
-
-for i in range(0, (1<<n)):       # 1 << n: 부분집합의 개수
-    for j in range(0, n):        # 원소의 수 만큼 비트를 비교함
-        if i & (1<<j):           # i의 j번째 비트가 1이면 j번째 원소 출력
-        # ↑ i의 j번 비트가 1인지 검사하는 코드
-            print('%d'%arr[j], end='')
-    print()
