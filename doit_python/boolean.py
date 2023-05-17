5ba29695ea96a9fd48985ee0db667d82dc04d663
Gobro-s <gobro6366@gmail.com>
2023-05-17 23:59:28 +0900

    0517 boolean

---
+++ doit_python/boolean.py
@@ -0,0 +1,10 @@
+# 불 자료형 : 참(True) / 거짓(False) 판별 자료형.
+# 참과 거짓 두개의 값만 가능하다.
+a = True
+b = False
+print(type(a), type(b))
+# class 'bool', class'bool'
+
+print(1==1)   # True
+print(2>1)    # True
+print(2<1)    # False
