2e01878b811f89c3c74a43d83595f00f18e3527d
Gobro-s <gobro6366@gmail.com>
2023-05-18 23:22:15 +0900

    boolean 0517

--- doit_python/boolean.py
+++ doit_python/boolean.py
@@ -8,3 +8,31 @@ print(type(a), type(b))
 print(1==1)   # True
 print(2>1)    # True
 print(2<1)    # False
+
+a = [1, 2, 3, 4]
+while a:
+    print(a.pop())
+    # 4
+    # 3
+    # 2
+    # 1
+
+if []: # 만약 []가 참이면
+    print('참')
+else:
+    print('거짓')
+# 거짓
+
+if [1, 2, 3, 4]:
+    print('참')
+else:
+    print('거짓')   # 참
+
+print(bool('python'))  # True
+print(bool(''))  # False
+print(bool({}))  # False
+print(bool(()))  # False
+print(bool([1, 2, 3]))  # True
+print(bool([]))  # False
+print(bool(0))  # False
+print(bool(5))  # True
