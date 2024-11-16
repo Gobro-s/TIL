325874fe5635785c2213716d8799037f438bb20c
Ko Sehoon <122508622+Gobro-s@users.noreply.github.com>
2024-11-16 17:32:57 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+board = [list(map(int,input().rstrip())) for _ in range(N)]
+board_cnt =  [[0]*M for _ in range(N)]
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+def BFS(a,b):
+    dq = deque()
+    dq.append((a,b))
+    board[a][b] = 0
+    board_cnt[a][b] = 1
+    
+    while dq:
+        p = dq.popleft()
+        
+        if p[0] == N-1 and p[1] ==M-1:
+            return board_cnt[p[0]][p[1]]
+
+
+        for i in range(4):
+            x = p[0]+dx[i]
+            y = p[1]+dy[i]
+            if 0<=x<N and 0<=y < M and board[x][y] == 1:
+                board[x][y] = 0
+                board_cnt[x][y] = board_cnt[p[0]][p[1]]+1
+                dq.append((x,y))
+                
+print(BFS(0,0))
