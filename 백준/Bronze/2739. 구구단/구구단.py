b2ac5f085cb9e5e9ea7144d66333321c7b7a1a69
Ko Sehoon <122508622+Gobro-s@users.noreply.github.com>
2024-11-20 21:54:07 +0900

    [Bronze V] Title: 구구단, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.py
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -1,10 +1,5 @@
-N = int(input())
-print(f'{N} * 1 =',N*1)
-print(f'{N} * 2 =',N*2)
-print(f'{N} * 3 =',N*3)
-print(f'{N} * 4 =',N*4)
-print(f'{N} * 5 =',N*5)
-print(f'{N} * 6 =',N*6)
-print(f'{N} * 7 =',N*7)
-print(f'{N} * 8 =',N*8)
-print(f'{N} * 9 =',N*9)
+import sys
+n = int(sys.stdin.readline())
+
+for i in range(1, 10):
+    print(f'{n} * {i} = {n*i}')
